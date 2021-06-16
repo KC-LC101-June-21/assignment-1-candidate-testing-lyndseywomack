1006b4917c5a5014647372e372358a76d1f9e67b
lyndseywomack <84876469+lyndseywomack@users.noreply.github.com>
2021-06-16 23:11:29 +0000

    Assignment #1 Part 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,6 +12,8 @@ let candidateAnswer = (" ")
 let questions = ["Who was the first American woman in space?	", "True or false: 5 kilometer == 5000 meters?	", "(5 + 3)/2 * 10 = ?	", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?	", "What is the minimum crew size for the ISS?	"]
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
 let candidateAnswers = []
+let numberOfCorrectAnswers = 0
+
 
 
 function askForName() {
@@ -26,30 +28,34 @@ candidateAnswers.push(candidateAnswer);
 }
 
 }
-
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-// if (candidateAnswer === correctAnswer){
-//   console.log("You're so smart!");
-// } else {
-//   console.log("Good try! Maybe google it and come back later... :)" );
-// }
+
 for (let i=0; i<questions.length; i++){
-  console.log(`${i+1}. ${questions[i]} \n Your Answer: ${candidateAnswers[i]} \n The Correct Answer: ${correctAnswers[i]}`);
+  console.log("Candidate Name: ", candidateName);
+  console.log(`${i+1}) ${questions[i]} \n Your Answer: ${candidateAnswers[i]} \n The Correct Answer: ${correctAnswers[i]}`);
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+      numberOfCorrectAnswers = numberOfCorrectAnswers + 1;
+  } 
 }
-
-
-  let grade;
   
+  let grade = (numberOfCorrectAnswers / correctAnswers.length * 100)
+  console.log(`>>>  Overall Grade: ${grade}% (${numberOfCorrectAnswers} out of 5 correct)  <<<`);
+  if (grade >= 80){
+    console.log('>>>  STATUS: Passed  <<<')
+  } else {
+    console.log('>>>  STATUS: Failed  <<<')
+  }
+  return grade
 
-  return grade;
 }
+
+
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Hey there, " + candidateName + " how you doin?");
+  // console.log("Hey there, " + candidateName + " please answer the questions below.");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
@@ -67,3 +73,4 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
+
