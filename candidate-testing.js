2368c21ff86f486f6519dc5ac82acc5f75f87a0f
lyndseywomack <84876469+lyndseywomack@users.noreply.github.com>
2021-06-17 00:11:43 +0000

    changed ask for name

--- candidate-testing.js
+++ candidate-testing.js
@@ -16,7 +16,7 @@ let numberOfCorrectAnswers = 0
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("Candidate Name: ");
+candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
